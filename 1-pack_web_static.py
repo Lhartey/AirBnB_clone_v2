378d111713462828a1a7bfaa2f51f8631d1e24f1
Augustine Lartey <augustinelartey68@gmail.com>
2023-05-07 12:30:44 -0700

    Fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,22 +1,34 @@
 #!/usr/bin/python3
-# Fabfile to generates a .tgz archive from the contents of web_static.
-import os.path
-from datetime import datetime
+"""
+Fabric script that generates a .tgz archive from the contents
+of the web_static folder of your AirBnB Clone repo.
+"""
+
 from fabric.api import local
+from datetime import datetime
+import os
 
 
 def do_pack():
-    """Create a tar gzipped archive of the directory web_static."""
-    dt = datetime.utcnow()
-    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
-                                                         dt.month,
-                                                         dt.day,
-                                                         dt.hour,
-                                                         dt.minute,
-                                                         dt.second)
-    if os.path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
-            return None
-    if local("tar -cvzf {} web_static".format(file)).failed is True:
+    """
+    Compress the contents of the web_static folder into a .tgz archive.
+    Return the path to the archive if successful, None otherwise.
+    """
+    # Create the versions directory if it doesn't already exist
+    if not os.path.exists("versions"):
+        os.mkdir("versions")
+
+    # Create the name of the archive with the current timestamp
+    now = datetime.now()
+    timestamp = now.strftime("%Y%m%d%H%M%S")
+    archive_name = "web_static_{}.tgz".format(timestamp)
+
+    # Use tar to create the archive
+    result = local("tar -cvzf versions/{} web_static".format(archive_name))
+
+    # Check if the archive was created successfully and return the path
+    if result.succeeded:
+        return "versions/{}".format(archive_name)
+    else:
         return None
-    return file
+
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+env.hosts = ["18.207.233.148", "54.237.21.109"]
 
 
 def do_pack():
